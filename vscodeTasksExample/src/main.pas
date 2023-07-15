15f19e921a00ec1efb9c1b05b900f28434540eef
John Lienau <zitrie@gmx.de>
2023-07-16 01:15:28 +0200

    Chance to folder later

--- vscodeTasksExample/src/main.pas
+++ vscodeTasksExample/src/main.pas
@@ -2,6 +2,29 @@ program main;
 
 uses testUtils;
 
+var i:integer;
 begin
-    calcFractionForNTimes(1000);
+    // calcFractionForNTimes(1000);
+
+    // programmieren 2 sose2020
+    // aufgabe 1
+    // aufgabe1Rekru(10);
+    for i := 0 to 10 do
+    begin
+        // writeln(aufgabe1Rekru(i));
+        // writeln(aufgabe1EndRekru(i));
+        writeln(aufgabe1Iteration(i));
+    end;
+
+    // aufgabe 2
+    // 9 == 1001
+    i := 9;
+    writeln(i and 0);
+    writeln(i and 2);
+    writeln(i or 0);
+    writeln(i or 2);
+    writeln(i xor 0);
+    writeln(i xor 2);
+    writeln(not i);
+    //
 end.
--- vscodeTasksExample/src/testUtils.pas
+++ vscodeTasksExample/src/testUtils.pas
@@ -6,7 +6,22 @@ interface
     procedure calcFractionForNTimes(a:integer);
     function calcFractions(a:integer):integer;
 
+    // programmieren 2 sose 2020
+    function aufgabe1Rekru(n:integer):integer;
+    function aufgabe1EndRekru(n:integer):integer;
+    function aufgabe1Iteration(n:integer):integer;
+
 implementation
+    type TPerson = record
+        name:string;
+        age:byte;
+    end;
+
+    type TP_PersonNode = ^TList;
+    type TPersonNode = record
+        person:TPerson;
+        next:TP_PersonNode;
+    end;
 
     procedure testFunc();
     var test:integer;
@@ -31,5 +46,46 @@ implementation
                 fraction := fraction + 1;
 
         calcFractions := fraction;
+    end;
+
+    function aufgabe1Rekru(n:integer):integer;
+    begin
+        if n <= 0 then
+            aufgabe1Rekru := 1
+        else
+            aufgabe1Rekru := aufgabe1Rekru(n - 1) * n;
+    end;
+
+    function aufgabe1EndRekru(n:integer):integer;
+    begin
+    end;
+
+    function aufgabe1Iteration(n:integer):integer;
+    begin
+        aufgabe1Iteration := 1;
+        while (n > 0) do
+        begin
+            aufgabe1Iteration := aufgabe1Iteration * n;
+            dec(n);
+        end;
+    end;
+
+    // todo test
+    procedure SwitchElements(p_personNode:TP_PersonNode);
+    var p_prevPersonNode:TP_PersonNode;
+    var tempPerson:TPerson;
+    begin
+        if (p_personNode.next = not nil) then
+        begin
+            p_personNode := p_personNode.next;
+            p_prevPersonNode := p_personnode;
+            p_personNode := p_personNode.next;
+            if (p_personNode = not nil) then
+            begin
+                tempPerson := p_prevPersonNode.person;
+                p_prevPersonNode.person := p_personNode.person;
+                p_personNode.person := tempPerson;
+            end;
+        end;
     end;
 end.
